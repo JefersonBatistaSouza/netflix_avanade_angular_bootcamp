6ea78fffbe0b2a6706569d450a468cc4b9c4041d
Jeferson Batista Souza <54870998+JefersonBatistaSouza@users.noreply.github.com>
2021-02-03 21:43:45 -0400

    Redirecionamento corrigido

--- assets/js/autenticar.js
+++ assets/js/autenticar.js
@@ -2,7 +2,7 @@ function autenticar(nome_telefone, senha){
     /*Exemple de criptografia em javascript*/
     var hash = md5('netflixclone');
     if((nome_telefone == "netflix") && md5(senha) == hash){
-        window.location.href = "/netflix";
+        window.location.href = "./netflix/index.html";
     }else{
         alert("Senha ou Usuario Incorreto\nUsuario:netflix\nSenha: netflixclone\nTudo em Minúsculo");
     }
